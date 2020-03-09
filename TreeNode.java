ed821298cd2ce340af0c87e767f81698e51e25e8
Srivastava <srivasrrahul@gmail.com>
2020-03-09 20:08:39 +0530

    More algorithms

--- TreeNode.java
+++ TreeNode.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
 }
